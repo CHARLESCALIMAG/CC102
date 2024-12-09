734f6863eb80f3b75f54a818d77f7998daf8c24a
CHARLESCALIMAG <nelsoncharlees@gmail.com>
2024-12-09 10:34:05 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION" << endl;
+	cout << "COLLEGE OF COMPUTER STUDIES" << endl;
+	cout << "BSIT 1 B" << endl;
+	cout << "CHARLES NELSON R. CALIMAG" << endl;
+	cout << "P-3 PINAGTAMBANGAN LABO CAMRINES NORTE" << endl;
+	
+	return 0;
+}
