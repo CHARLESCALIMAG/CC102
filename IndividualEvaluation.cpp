d0d4228a45ee31d53ec64a988cb1262dd9bdd7b6
CHARLESCALIMAG <nelsoncharlees@gmail.com>
2024-12-18 15:04:04 +0800

    Add files via upload

--- IndividualEvaluation.cpp
+++ IndividualEvaluation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
@@ -9,7 +10,10 @@ int main()
 			cout << "  "; 
 		}
 		for(int j = i; j < 8; j++){
-			if((i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 0 )){
+			if (j == 3 && i == 3 || j == 5 && i == 4 || j == 5 && i == 2 || j == 7 && i == 3){
+				cout << "  ";
+			}
+			else if((i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 0 )){
 				cout << "# ";
 			}
 			else if ((i % 2 == 0 && j % 2 == 0 || i % 2 == 1 &&  j % 2 == 1 )){
